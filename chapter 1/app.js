174fd6456767ae0babf0c0076bba239a3503e80a
usamalearner <syedusamalearner@gmail.com>
2020-06-15 12:51:42 +0500

    second commit

--- chapter 1/app.js
+++ chapter 1/app.js
@@ -1,5 +1,69 @@
-1
-alert("Greetings welcome");
-2
-alert("Error! please enter a valid password");
+//================================================================= chapter  1 ================
+// alert("Greetings welcome");
+// 
+// alert("Error! please enter a valid password");
+// 
+// alert("welcome to jS land" +"\r\n"+ "happy coding");
+// 
+// alert("welcome to jS land");
+// alert("happy coding");
+// 
+// ===========================================================chapter 2===================
+// var username = "usama ali";
+// 
+// var myName = "Syed Usama Ali";
+// 
+// var message = "Hello World";
+// alert(message);
+// 
+//  var name ="Usama";
+//  var age ="19 years old";
+//  var course =  "web and mobile hybrid app development";
+// alert(name);
+// alert(age);
+// alert(course);
+// 
+// var email = "syedusamaali@gmail.com";
+// alert("my email is " + email);
+// 
+// var book ="a smarter way way to learn JS";
+//  alert("I am trying to learn from a book" + book);
+// 
+// document.write("<h1>yeah I can write HTML content thriugh JS");
+// 
+// ==================================== chapter 3 =============================
+// var age = 15;
+//  alert("my age is" + age);
+// 
+// var times = "N times";
+//  alert("you have visited this site " + times);
+// 
+// var birthyear = 1999;
+// document.write(birthyear + "<br>" + "this type of my declared variable is number");
+// 
+// var name = "Syed Usama Ali";
+// var product = "T Shirts";
+// var quantity = 5;
+// document.write(name + "has ordered " + quantity + product);
+// 
+// ======================================chapter 4============================================
+// var a = "usama", age = 5, course = "WEB";
+// 
+//  legal varible Names
+// var userName , age, emailAddress, myName, yourName ;
+//  illegal variable names
+// var _usama , Name , MYname , AGe , CurrentLOcation;
+// 
+// document.write("<h1>Rules for naming JS variables</h1><br>");
+// document.write("Variable names can only contain numbers, $ and _  For example $my_1stVariable<br>" );
+// document.write("Variables must begin with a letter,$ or _ or . For example $name, _name or name <br>");
+// document.write("Variable names are case sensitive <br> Variable names should not be JS keywords");
+// 
+// ===============================================================chapter 5 ========================
+// 
+
+
+
+
+
 
